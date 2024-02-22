53d969d4fb43971d2b96dafe74ba0ab3107041dd
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-02-22 16:25:18 +0100

    Form

--- src/App.js
+++ src/App.js
@@ -8,8 +8,14 @@ import Navbar from './components/navbar/Navbar';
 import "bootstrap/dist/css/bootstrap.css";
 import LoginForm from './components/login/LoginForm';
 import MyQuestPage from './components/guild/MyQuestsPage';
+import { atom } from 'jotai';
+
+export const client = atom({});
 
 function App() {
+
+  
+
   return (
 
     <>
@@ -18,7 +24,7 @@ function App() {
         <Routes>
           <Route path=''element={<AllQuests/>}></Route>
           <Route path='/loginform'element={<LoginForm/>}></Route>
-          <Route path='/myquest'element={<MyQuestPage/>}></Route>
+          <Route path='/myquests'element={<MyQuestPage/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,22 +1,51 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { client } from "../../App";
+import { useAtom } from 'jotai';
+import { useEffect, useState } from "react";
 
 export default function LoginForm()
 {
+    const navigate = useNavigate();
+    const [data, setData] = useAtom(client);
+    const [login,setLogin] = useState(
+            {
+                name:"",
+                authentication_seal:""
+            }
+        );
+
+    function synchronize(e)
+    {
+        let clone = {...login};
+        clone[e.target.name] = e.target.value;
+        setLogin(clone);
+        
+    }
+
+    function sendForm()
+    {
+        axios.post("/guilds/login",login).then(
+            (response)=>
+            {
+                setData(response.data);
+                navigate("/");
+            }
+        )//questo manda una post avente come body pers JSONIZZATO
+    }
+
+
     return(
         <div className="container ">
             <form>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Create Date</label>
-                    <input type="date" className="form-control" id="date" aria-describedby="emailHelp"/>
-                </div>
-
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Status</label>
-                    <input type="text" className="form-control" id="status" aria-describedby="emailHelp"/>
+                    <input name="name" type="date" className="form-control" id="date" aria-describedby="emailHelp" value={login.name} onChange={synchronize}/>
                 </div>
 
                 <div className="mb-3">
                     <label for="exampleInputPassword1" className="form-label">Rank</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <input name="authentication_seal" type="password" className="form-control" id="exampleInputPassword1" aria-describedby="emailHelp" value={login.authentication_seal} onChange={synchronize}/>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Login</button>

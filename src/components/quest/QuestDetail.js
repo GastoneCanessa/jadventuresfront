3413911df8a35250419d11bed5660f6e8cc7384f
Alessandro <irelip.a@gmail.com>
2024-02-22 15:22:06 +0100

    v1

--- src/components/quest/QuestDetail.js
+++ src/components/quest/QuestDetail.js
@@ -1,20 +1,19 @@
-//tutti i campi
-export default function QuestDetail()
-{
-    return(
-        <>
-            <div className="card" style={{width: "18rem"}}>
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{type}</h5>
-                    <p className="card-text">{date_created}</p>
-                    <p className="card-text">{status}</p>
-                    <p className="card-text">{description}</p>
-                    <p className="card-text">{rank}</p>
-                    <p className="card-text">{date}</p>
-                    <p className="card-text">{reward}</p>
-                </div>
+import React from 'react';
+
+export default function QuestDetail({ quest }) {
+    const { type, date_created, status, description, rank, date_completed, reward } = quest;
+
+    return (
+        <div className="card shadow mb-4">
+            <div className="card-body">
+                <h5 className="card-title">Type: {type}</h5>
+                <p className="card-text">Date Created: {date_created}</p>
+                <p className="card-text">Status: {status}</p>
+                <p className="card-text">Description: {description}</p>
+                <p className="card-text">Rank: {rank}</p>
+                <p className="card-text">Date Completed: {date_completed}</p>
+                <p className="card-text">Reward: {reward}</p>
             </div>
-        </>
+        </div>
     );
 }

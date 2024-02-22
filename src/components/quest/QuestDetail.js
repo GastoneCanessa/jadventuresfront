47834960ac79eb1165f2b00519a7591987608930
“Cristina <coniglicristina@gmail.com>
2024-02-22 14:02:20 +0100

    strutturato

--- src/components/quest/QuestDetail.js
+++ src/components/quest/QuestDetail.js
@@ -1,1 +1,20 @@
 //tutti i campi
+export default function QuestDetail()
+{
+    return(
+        <>
+            <div className="card" style={{width: "18rem"}}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{type}</h5>
+                    <p className="card-text">{date_created}</p>
+                    <p className="card-text">{status}</p>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text">{rank}</p>
+                    <p className="card-text">{date}</p>
+                    <p className="card-text">{reward}</p>
+                </div>
+            </div>
+        </>
+    );
+}

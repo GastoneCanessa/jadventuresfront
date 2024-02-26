0088ae4d8fbbae286a4bd982ec4b20d4b120c294
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-02-26 12:30:34 +0100

    Logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -21,12 +21,12 @@ export default function Navbar() {
         setData({});
         localStorage.setItem('clientState', JSON.stringify({}));
         navigate("/");
-         // Aggiorna il localStorage direttamente
+        // Aggiorna il localStorage direttamente
     }
 
     function logOutParty() {
         setPart({});
-         // Aggiorna il localStorage direttamente
+        // Aggiorna il localStorage direttamente
         localStorage.setItem('partyState', JSON.stringify({}));
         navigate("/");
 
@@ -37,76 +37,84 @@ export default function Navbar() {
     return (
 
         <>
-            <nav className="sticky-top text-white mb-4 navbar navbar-expand-lg" style={{ backgroundColor: "#597E52" }} >
-                <div className="container">
+            <nav className="sticky-top text-white mb-4 navbar navbar-expand-lg d-flex justify-content-between px-4 py-0" style={{ backgroundColor: "#597E52" }} >
 
-                    <Link className="text-primary navbar-brand" to="homepage"></Link>
-                    <ul className="text-white navbar-nav">
+                <img
+                    className="p-0 rounded-4 m-1"
+                    style={{ width: "75px", height: "60px" }}
+                    src="/we.webp"
+                    alt="Logo"
+                    onClick={logOut}
+                />
 
 
-                        {!isPartaNotEmpty &&
-                            (
-                                <>
-                                    <li className="nav-item">
-                                        <Link className="text-white  nav-link active" to="/">All Quests</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="text-white  nav-link active" to="/loginparty">Login Party</Link>
-                                    </li>
-                                </>
-                            )
-                        }
-                        {!isDataNotEmpty &&
-                            (
-                                <>
-                                    <li className="nav-item">
-                                        <Link className="text-white  nav-link active" to="/loginguild">Login Guild</Link>
-                                    </li>
-                                </>
-                            )
-                        }
+                <Link className="text-primary navbar-brand" to="homepage"></Link>
+                <ul className="text-white navbar-nav">
 
-                        {isPartaNotEmpty && (
+                    {!isPartaNotEmpty &&
+                        (
                             <>
                                 <li className="nav-item">
-                                    <Link className="text-white nav-link active" to={"/"} >Available Quests</Link>
+                                    <Link className="text-white  nav-link active px-2" to="/"><h4>All Quests</h4></Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="text-white nav-link active border me-1" to="/">{part.name}</Link>
-                                </li>
-                                <li className="nav-item">
-                                    {/* <Link className="text-white nav-link active" to="/" onClick={logOut}>Seal image</Link> */}
-                                    <Link to="" className="navbar-brand text-white" onClick={logOutParty}>
-                                        Log Out
-                                    </Link>
+                                    <Link className="text-white  nav-link active" to="/loginparty"><h4>Login Party</h4></Link>
                                 </li>
                             </>
                         )
-                        }
-                        {isDataNotEmpty && (
+                    }
+                    {!isDataNotEmpty &&
+                        (
                             <>
                                 <li className="nav-item">
-                                    <Link className="text-white nav-link active" to={"/myquests/byguild/" + data.id} >My Quests</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="text-white nav-link active border me-1" to="/">{data.name}</Link>
-                                </li>
-                                <li className="nav-item">
-                                    {/* <Link className="text-white nav-link active" to="/" onClick={logOut}>Seal image</Link> */}
-                                    <Link to="" className="navbar-brand">
-                                        <img
-                                            style={{ width: "50px", height: "40px" }}
-                                            src={data.seal_img_url}
-                                            alt="Logo"
-                                            onClick={logOut}
-                                        />
-                                    </Link>
+                                    <Link className="text-white  nav-link active px-2" to="/loginguild"><h4>Login Guild</h4></Link>
                                 </li>
                             </>
                         )
-                        }
-                    </ul>
-                </div>
+                    }
+
+                    {isPartaNotEmpty && (
+                        <>
+                            <li className="nav-item">
+                                <Link className="text-white nav-link active px-2" to={"/"} ><h4>Available Quests</h4></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="text-white nav-link active border me-1" to="/">{part.name}</Link>
+                            </li>
+                            <li className="nav-item">
+                                {/* <Link className="text-white nav-link active" to="/" onClick={logOut}>Seal image</Link> */}
+                                <Link to="" className="navbar-brand text-white" onClick={logOutParty}>
+                                    Log Out
+                                </Link>
+                            </li>
+                        </>
+                    )
+                    }
+                    {isDataNotEmpty && (
+                        <>
+                            <li className="nav-item">
+                                <Link className="text-white nav-link active px-2" to={"/myquests/byguild/" + data.id} ><h4>My Quests</h4></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="text-white nav-link active border me-1" to="/">{data.name}</Link>
+                            </li>
+                            <li className="nav-item">
+                                {/* <Link className="text-white nav-link active" to="/" onClick={logOut}>Seal image</Link> */}
+                                <Link to="" className="navbar-brand">
+                                    <img
+                                        style={{ width: "50px", height: "40px" }}
+                                        src={data.seal_img_url}
+                                        alt="Logo"
+                                        onClick={logOut}
+                                    />
+                                </Link>
+                            </li>
+                        </>
+                    )
+                    }
+
+                </ul>
+
             </nav>
         </>
     );

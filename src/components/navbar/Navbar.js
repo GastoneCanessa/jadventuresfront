b37d562dc060ff223ee77be9ad8c00c85143c1ad
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-02-26 09:12:04 +0100

    v2

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,7 +5,6 @@ import { party } from "../../App";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useEffect, useState } from "react";
 
 export default function Navbar() {
 
